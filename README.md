4dbc6aacbe665773268f86e3919cc8ddb057a872
Newas <newas.azad@bjss.com>
2021-10-13 06:06:40 -0400

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# newas
+# Newas Azad
+
+# Problem
+### Write a function that accepts two or more number values and returns the average of those values. Values that are string representations of numbers should be converted to numbers and included in the average as well. The function should throw an error if any given value is not a number or cannot be converted to a number.
+
+# How To Run
+### Copy and paste functions and test cases into google chrome browser
+### You can also run test cases locally by installing the Code Runner plugin in VS Code
+
---
+++ exercise.js
@@ -0,0 +1,36 @@
+/* Requirements:
+ * 1. can only accept 2 or more arguments 
+ * 2. arguments can be of type number and string (string representation of numbers)
+ * 3. throw error if argument is NOT a number 
+ * 4. throw error if argument cannot be converted to a number
+ * 5. return average
+*/
+
+
+const getNumAvg = function(...arguments) {
+    let args = [...arguments];
+    let newArr = [];
+
+    try {
+        if(args.length < 2) throw `Must enter two or more values!`; 
+        for(let i=0; i<args.length; i++) {
+            if(typeof(args[i]) === 'number') newArr.push(args[i]);
+            else if(typeof(args[i]) === 'string') {
+                if(isStringNumeric(args[i])) newArr.push(parseInt(args[i]));
+                else throw `${args[i]} is not a valid number!`;
+            }
+            else return `${args[i]} is not a valid value`;
+        }
+        return sum(newArr) / args.length;
+    }catch(e) {
+        return e;
+    }
+}
+
+const sum = (arr) => arr.reduce((acc, currval) => acc + currval, 0);
+
+const isStringNumeric = str => {
+    if([...str].length < 1) return false;
+    return [...str].every(num => '0123456789'.includes(num));
+}
+   
