e108195dd2b1127f97cf76ebb32d95ea8f9a8de8
Newas <newas.azad@bjss.com>
2021-10-18 01:11:27 -0400

    refactor code

--- exercise.js
+++ exercise.js
@@ -1,27 +1,18 @@
-/* Requirements:
- * 1. can only accept 2 or more arguments 
- * 2. arguments can be of type number and string (string representation of numbers)
- * 3. throw error if argument is NOT a number 
- * 4. throw error if argument cannot be converted to a number
- * 5. return average
-*/
-
-const getNumAvg = function() {
-    let args = [...arguments];
+const getNumAvg = function(...args) {
     let newArr = [];
 
     try {
         if(args.length < 2) throw `Must enter two or more values!`; 
         for(let i=0; i<args.length; i++) {
             if(typeof(args[i]) === 'number') newArr.push(args[i]);
-            else if(typeof(args[i]) === 'string' && [...args[i]].length > 0) {
-                if(isStringNumeric(args[i])) newArr.push(parseInt(args[i]));
-                else throw `${args[i]} is not a valid number!`;
+            else if(typeof(args[i]) === 'string') {
+                if(isNaN(args[i])) throw `${args[i]} is not a valid number!`;
+                else newArr.push(parseFloat(args[i]));
             }
             else return `${args[i]} is an invalid value!`;
         }
-        return sum(newArr) / args.length;
-    }catch(e) {
+        return sum(newArr) / newArr.length;
+    } catch(e) {
         return e;
     }
 }
@@ -30,12 +21,6 @@ const sum = function(array) {
     return array.reduce((acc, currval) => {
         return acc + currval
     }, 0)
-}
-
-const isStringNumeric = function(string) {
-    return [...string].every(num => {
-        return '0123456789'.includes(num)
-    })
 }
 
 module.exports = {
--- exercise.test.js
+++ exercise.test.js
@@ -1,64 +1,76 @@
 const { expect } = require('@jest/globals')
 const { getNumAvg } = require('./exercise')
 
-test('should not accept less than 2 values', () => {
-    const result1 = getNumAvg(15)
-    const result2 = getNumAvg()
-
-    expect(result1).toBe('Must enter two or more values!')
-    expect(result2).toBe('Must enter two or more values!')
-})
-
-test('should return average for valid numeric args', () => {
-    const result1 = getNumAvg(2, 3)
-    const result2 = getNumAvg(-20, -5)
-
-    expect(result1).toEqual(2.5)
-    expect(result2).toEqual(-12.5)
-})
-
-test('should return average for valid string args', () => {
-    const result1 = getNumAvg('13', '2')
-    const result2 = getNumAvg('1024', '258')
-
-    expect(result1).toEqual(7.5)
-    expect(result2).toEqual(641)
-})
-
-test('should return average for valid numeric and string args', () => {
-    const result1 = getNumAvg('7', 8)
-    const result2 = getNumAvg(9, '62')
-    const result3 = getNumAvg(1, 7, '48', 52)
-
-    expect(result1).toBe(7.5)
-    expect(result2).toBe(35.5)
-    expect(result3).toBe(27)
-})
-
-test('should not accept invalid strings', () => {
-    const result1 = getNumAvg('7x890', 99)
-    const result2 = getNumAvg('Xyz83', 100)
-    const result3 = getNumAvg('92aBc', '44')
-    const result4 = getNumAvg('$@#$_^', '23')
-
-    expect(result1).toBe('7x890 is not a valid number!')
-    expect(result2).toBe('Xyz83 is not a valid number!')
-    expect(result3).toBe('92aBc is not a valid number!')
-    expect(result4).toBe('$@#$_^ is not a valid number!')
-})
-
-test('should throw error if arg data type is not a number or string', () => {
-    const result1 = getNumAvg(null, null)
-    const result2 = getNumAvg(undefined, undefined)
-    const result3 = getNumAvg([12, 24, '36', '48'], ['5', '10', 15, 20])
-    const result4 = getNumAvg({"a":1, "b": 2}, {"c":'77', "d":'88'})
-    const result5 = getNumAvg(true, true)
-    const result6 = getNumAvg(false, false)
-
-    expect(result1).toBe('null is an invalid value!')
-    expect(result2).toBe('undefined is an invalid value!')
-    expect(result3).toBe('12,24,36,48 is an invalid value!')
-    expect(result4).toBe('[object Object] is an invalid value!')
-    expect(result5).toBe('true is an invalid value!')
-    expect(result6).toBe('false is an invalid value!')
+describe('Exercise.js', () => {
+    test('should not accept less than 2 values', () => {
+        const result1 = getNumAvg(15)
+        const result2 = getNumAvg()
+    
+        expect(result1).toBe('Must enter two or more values!')
+        expect(result2).toBe('Must enter two or more values!')
+    })
+    
+    test('should return average for valid numeric args', () => {
+        const result1 = getNumAvg(2, 3)
+        const result2 = getNumAvg(-20, -5)
+        const result3 = getNumAvg(2.5, 3.5, 10)
+    
+        expect(result1).toEqual(2.5)
+        expect(result2).toEqual(-12.5)
+        expect(result3).toEqual(5.333333333333333)
+    })
+    
+    test('should return average for valid string args', () => {
+        const result1 = getNumAvg('13', '2')
+        const result2 = getNumAvg('1024', '258')
+        const result3 = getNumAvg('10.32', '6.78', '11.99')
+    
+        expect(result1).toEqual(7.5)
+        expect(result2).toEqual(641)
+        expect(result3).toEqual(9.696666666666667)
+    })
+    
+    test('should return average for valid numeric and string args', () => {
+        const result1 = getNumAvg('7', 8)
+        const result2 = getNumAvg(9, '62')
+        const result3 = getNumAvg(1, 7, '48', 52)
+        const result4 = getNumAvg(38.77, 162, '38.12', 45)
+    
+        expect(result1).toBe(7.5)
+        expect(result2).toBe(35.5)
+        expect(result3).toBe(27)
+        expect(result4).toBe(70.9725)
+    })
+    
+    test('should not accept invalid strings', () => {
+        const result1 = getNumAvg('7x890', 99)
+        const result2 = getNumAvg('Xyz83', 100)
+        const result3 = getNumAvg('92aBc', '44')
+        const result4 = getNumAvg('$@#$_^', '23')
+        const result5 = getNumAvg('', '')
+        const result6 = getNumAvg(' ', ' ')
+    
+        expect(result1).toBe('7x890 is not a valid number!')
+        expect(result2).toBe('Xyz83 is not a valid number!')
+        expect(result3).toBe('92aBc is not a valid number!')
+        expect(result4).toBe('$@#$_^ is not a valid number!')
+        expect(result5).toBe(NaN)
+        expect(result6).toBe(NaN)
+    })
+    
+    test('should throw error if arg data type is not a number or string', () => {
+        const result1 = getNumAvg(null, null)
+        const result2 = getNumAvg(undefined, undefined)
+        const result3 = getNumAvg([12, 24, '36', '48'], ['5', '10', 15, 20])
+        const result4 = getNumAvg({"a":1, "b": 2}, {"c":'77', "d":'88'})
+        const result5 = getNumAvg(true, true)
+        const result6 = getNumAvg(false, false)
+    
+        expect(result1).toBe('null is an invalid value!')
+        expect(result2).toBe('undefined is an invalid value!')
+        expect(result3).toBe('12,24,36,48 is an invalid value!')
+        expect(result4).toBe('[object Object] is an invalid value!')
+        expect(result5).toBe('true is an invalid value!')
+        expect(result6).toBe('false is an invalid value!')
+    })
 })
